6b3ad2dee1c481baf332ec16f6e91b54918b6c7f
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-29 07:44:38 +0530

    component 6 added in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from "react";
-import	ViewNumbers from './ViewNumbers';
-import	ViewTopics from './ViewTopics';
-import	ViewTodos from './ViewTodos';
-import	AddNumbers from './AddNumbers';
-import	AddTopics from './AddTopics';
-import	AddTodos from './AddTodos';
-
+import React from 'react';
+import ViewNumbers from './ViewNumbers';
+import ViewTopics from './ViewTopics';
+import ViewTodos from './ViewTodos';
+import AddNumbers from './AddNumbers';
+import AddTopics from './AddTopics';
+import AddTodos from './AddTodos';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ViewNumbers />
+      <ViewTopics />
+      <ViewTodos />
+      <AddNumbers />
+      <AddTopics />
+      <AddTodos />
     </div>
   );
 }
