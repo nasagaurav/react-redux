88169c458b7df05a9f41b4a0d7b07b743793651f
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-29 07:49:27 +0530

    redux setup only completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+function reducer(state = { a: [], b: [], c: [], counter: 0 }, action) {
+  console.log('this is coming from reducer() in index.js', action);
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </StrictMode>
 );
